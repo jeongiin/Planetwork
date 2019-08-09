129c1d92c56b0d521ec21639999295423a515bc2
HeegeePark <phg980513@naver.com>
2019-08-09 21:47:41 +0900

    R->Wac intent

--- app/src/main/java/com/example/planetwork/ReadLetterActivity.kt
+++ app/src/main/java/com/example/planetwork/ReadLetterActivity.kt
@@ -1,12 +1,20 @@
 package com.example.planetwork
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_read_letter.*
 
 class ReadLetterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_read_letter)
+
+        btnResend.setOnClickListener{
+            val intent = Intent(this, WriteLetterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
